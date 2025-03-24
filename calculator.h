9e0fa7f3af16167628e24534ed3f8beb1adb283f
Ahmed Gamal <ahmedmgh67@gmail.com>
2025-03-24 15:46:44 +0200

    fix header

--- calculator.h
+++ calculator.h
@@ -2,25 +2,13 @@
 using namespace std;
 
 template <typename T1>
-T multiplication(T num1, T num2)
-{
-    return num1*num2;
-}
+T multiplication(T num1, T num2);
 
 template <typename T1>
-T division(T num1, T num2)
-{
-    if (num2==0)
-    return 0;
-    return num1/num2;
-}
+T division(T num1, T num2);
 
 template <typename T>
-T addition(T n1, T n2){
-	return n1+n2;
-}
+T addition(T n1, T n2);
 
 template <typename T>
-T substraction(T n1, T n2){
-	return n1-n2;
-}
+T substraction(T n1, T n2);
