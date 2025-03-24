53bacec6645400c3d8d8a0798559290971b171fc
Ahmed Gamal <ahmedmgh67@gmail.com>
2025-03-24 15:43:09 +0200

    add addition and sub to header

---
+++ calculator.h
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+template <typename T>
+T addition(T n1, T n2){
+	return n1+n2;
+}
+
+template <typename T>
+T substraction(T n1, T n2){
+	return n1-n2;
+}
